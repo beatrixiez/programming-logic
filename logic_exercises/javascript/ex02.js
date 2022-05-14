37dfadf2d30065e1d87fda57c8b073333e8e2a01
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:29:23 -0300

    file updated

--- logic_exercises/javascript/ex02.js
+++ logic_exercises/javascript/ex02.js
@@ -1,7 +1,10 @@
 let numero1, numero2, subtracao;
 
 alert('Vamos realizar uma subtração, pressione ENTER ou OK')
+
 numero1 = parseInt(prompt('Digite um número: '));
 numero2 = parseInt(prompt('Digite o valor a ser subtraído: '));
+
 subtracao = (numero1 = numero2)
+
 alert('O total da subtração é: ' + subtracao)
