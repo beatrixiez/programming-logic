f8a8584a905901ce2a36a0c3433958fad6405adb
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:29:47 -0300

    file updated

--- logic_exercises/javascript/ex01.js
+++ logic_exercises/javascript/ex01.js
@@ -1,7 +1,10 @@
 let numero1, numero2, soma;
 
 alert('Vamos realizar uma soma, pressione ENTER ou OK')
+
 numero1 = parseInt(prompt('Digite um número: '));
 numero2 = parseInt(prompt('Digite outro número: '));
+
 soma = (numero1 + numero2)
+
 alert('A soma total é: ' + soma)
