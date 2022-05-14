f5111ecb9e86030ff11257b2331b7f4b489fa4fc
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:28:01 -0300

    file updated

--- logic_exercises/javascript/ex06.js
+++ logic_exercises/javascript/ex06.js
@@ -1,6 +1,8 @@
 let numero, sucessor;
 
 alert('Vamos descobrir o sucessor de um número, pressione ENTER ou OK')
+
 numero = parseInt(prompt("Digite um número: "));
 sucessor = (numero + 1)
+
 alert("O sucessor é: " + sucessor)
