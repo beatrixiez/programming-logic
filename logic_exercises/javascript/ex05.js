b7492b5bc65960b6f9b72b3a52df3e1ba7967191
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:28:25 -0300

    file updated

--- logic_exercises/javascript/ex05.js
+++ logic_exercises/javascript/ex05.js
@@ -1,6 +1,8 @@
 let numero, antecessor;
 
 alert('Vamos descobrir o antecessor de um número, pressione ENTER ou OK')
+
 numero = parseInt(prompt("Digite um número: "));
 antecessor = (numero - 1)
+
 alert("O antecessor é: " + antecessor)
