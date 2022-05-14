f555362402dea5183f964201571a0041a393b357
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:27:29 -0300

    file updated

--- logic_exercises/javascript/ex07.js
+++ logic_exercises/javascript/ex07.js
@@ -1,7 +1,9 @@
 let base, altura, area;
 
 alert('Vamos descobrir o valor da área de um retângulo. Pressione ENTER ou OK')
+
 base = parseFloat(prompt('Digite o valor da base do retângulo: '));
 altura = parseFloat(prompt('Digite o valor da altura do retângulo: '));
 area = (base*altura)
+
 alert('A área do retângulo é: ' + area)
