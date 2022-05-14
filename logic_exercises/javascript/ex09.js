9fed3f8161a074e67ae0f3f9754854e654443e61
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:26:16 -0300

    file updated

--- logic_exercises/javascript/ex09.js
+++ logic_exercises/javascript/ex09.js
@@ -1,8 +1,11 @@
 let salarioAtual, reajuste, aumentoSalarial, salarioFinal;
 
 alert('Vamos calcular um reajuste salarial. Pressione ENTER ou OK')
+
 salarioAtual = parseFloat(prompt('Digite seu salário atual - não adicionar pontos e vírgulas: '));
 reajuste = parseFloat(prompt('Digite o percentual de reajuste - não adicionar o sinal de porcentagem %: '));
+
 aumentoSalarial = (reajuste / 100 * salarioAtual);
 salarioFinal = (aumentoSalarial + salarioAtual);
+
 alert('O salário final corresponde a: R$' + salarioFinal)
