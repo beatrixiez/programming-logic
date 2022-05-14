90db35cbae5b4af468bc21f3743fdcbb7b9bb188
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-05-14 20:30:09 -0300

    file updated

--- logic_exercises/javascript/ex03.js
+++ logic_exercises/javascript/ex03.js
@@ -1,7 +1,10 @@
 let numero1, numero2, multiplicacao;
 
 alert('Vamos realizar uma multiplicação, pressione ENTER ou OK')
+
 numero1 = parseInt(prompt("Digite um número: "));
 numero2 = parseInt(prompt("Digite outro número: "));
+
 multiplicacao = (numero1 * numero2)
+
 alert("O total da multiplicação é: " + multiplicacao)
